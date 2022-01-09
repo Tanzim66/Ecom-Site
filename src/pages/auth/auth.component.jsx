aac3f0d1e99cc3bab0845f381068f9cab2ea80f4
Tanzim66 <tanzim.ahmed@mail.utoronto.ca>
2022-01-08 19:37:06 -0500

    Finish sign in and sign up functionality
    Finish sign in and sign up functionality

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './auth.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const Auth = ()=>{
   return (
     <div className="auth">
       <SignIn/>
+      <SignUp />
     </div>
   );
 };
