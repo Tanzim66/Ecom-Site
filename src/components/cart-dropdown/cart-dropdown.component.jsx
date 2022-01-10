1cd4cece778779b4cddda906e62e41b5de1179d7
Tanzim66 <tanzim.ahmed@mail.utoronto.ca>
2022-01-10 14:23:46 -0500

    Update cart items count with reselect
    Update cart items count with reselect so that react does not rerender the count every time an action is fired.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import {selectCartItems} from '../../redux/cart/cart.select';
 
 import './cart-dropdown.styles.scss';
 
@@ -20,8 +21,8 @@ const CartDropdown = ({cartItems}) => {
   );
 };
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
