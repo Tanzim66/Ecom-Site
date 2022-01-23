13d5947f57e7e393f5b1a7121d830c873822fe2a
Tanzim66 <tanzim.ahmed@mail.utoronto.ca>
2022-01-23 02:19:30 -0500

    Complete collection page
    Complete collection page (hats, jackets, sneakers, womens, mens)

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,20 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 import React from 'react';
+import {Route} from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from
+  '../../components/collections-overview/collections-overview.component';
+import Collection from '../collection/collection.component';
 
-import CollectionPreview
-  from '../../components/collection-preview/collection-preview.component';
+const ShopPage = ({match}) => {
+  return (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+      <Route path={`${match.path}/:collectionId`} component={Collection}/>
+    </div>
+  );
+};
 
-class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state={
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop'>
-        {
-          collections.map(
-              ({id, ...otherCollectionProps}) =>
-                (<CollectionPreview key={id} {...otherCollectionProps}/>))
-        }
-      </div>
-    );
-  }
-}
 
 export default ShopPage;
